7afd0c8dcd9a7b3e9b10afc92f9b2dc1fba10280
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2026-04-20 21:25:46 -0400

    fixed gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugin: [react()],
   /* Required for GitHub Pages */
-  base: "/CSCE548-Project1/",
+  base: "/CSCE548-SemesterProject/",
 });
